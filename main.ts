c5422b12eeaa71d242e4a3a81ace884a3fb36b2d
pierreyvesbenner <75337276+pierreyvesbenner@users.noreply.github.com>
2020-12-05 11:50:43 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,43 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 1) {
-        kitronik_servo_lite.driveForwards(100)
+    if (receivedNumber == 8) {
         basic.showLeds(`
-            # # . # #
-            # # . # #
-            . . . . .
-            # . . . #
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
             . # # # .
             `)
+        kitronik_servo_lite.forward()
+    }
+    if (receivedNumber == 14) {
+        basic.showLeds(`
+            . # # # .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+        kitronik_servo_lite.backward()
+    }
+    if (receivedNumber == 12) {
+        basic.showLeds(`
+            . . # . .
+            . # . . #
+            # # # # #
+            . # . . #
+            . . # . .
+            `)
+        kitronik_servo_lite.left()
+    }
+    if (receivedNumber == 13) {
+        basic.showLeds(`
+            . . # . .
+            # . . # .
+            # # # # #
+            # . . # .
+            . . # . .
+            `)
+        kitronik_servo_lite.right()
     }
 })
 radio.setGroup(1)
