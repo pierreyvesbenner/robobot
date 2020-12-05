0daba2625c7a5c789a47d4bb9bc138f512329812
pierreyvesbenner <75337276+pierreyvesbenner@users.noreply.github.com>
2020-12-05 12:01:38 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 8) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
         basic.showLeds(`
             . . # . .
             . # # # .
@@ -39,5 +40,33 @@ radio.onReceivedNumber(function (receivedNumber) {
             `)
         kitronik_servo_lite.right()
     }
+    if (receivedNumber == 15) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        kitronik_servo_lite.right()
+    }
+    if (receivedNumber == 16) {
+        strip.showRainbow(1, 360)
+        basic.showLeds(`
+            # # # # #
+            # . # . #
+            # # . # #
+            # . # . #
+            # # # # #
+            `)
+        kitronik_servo_lite.right()
+        kitronik_servo_lite.backward()
+        kitronik_servo_lite.left()
+        kitronik_servo_lite.forward()
+        kitronik_servo_lite.stop()
+    }
 })
+let strip: neopixel.Strip = null
 radio.setGroup(1)
+strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
